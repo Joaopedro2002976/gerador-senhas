04a099b38f3732f9aca7b0ca93f4974419c2f1a8
João Pedro Resende <jp.juca@hotmail.com>
2026-05-27 10:07:37 -0300

    Add files via upload

---
+++ gerador de senhas.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def gerar_senha(tamanho):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+
+    return senha
+
+print("=== GERADOR DE SENHAS ===")
+
+tamanho = int(input("Digite o tamanho da senha: "))
+
+senha = gerar_senha(tamanho)
+
+print(f"\nSenha gerada: {senha}")
